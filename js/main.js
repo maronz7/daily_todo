a2b968444a1b1ad86bf9309c101c9f948b5d24cd
maronz7 <35907465+maronz7@users.noreply.github.com>
2020-11-20 22:08:12 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,10 +6,9 @@ const $allDeleteBtn = document.getElementById('all-delete');
 const $allResetBtn = document.getElementById('all-reset');
 
 
-// ########## 追加 ###########
+// 追加
 (function () {
     // 初期化処理
-    // ローカルストレージに格納されている値を取得し、リストを生成する
     for (var key in localStorage) {
         var html = localStorage.getItem(key);
         if (html) {
@@ -19,9 +18,7 @@ const $allResetBtn = document.getElementById('all-reset');
 })();
 
 const saveTaskToLocalStorage = (task, html) => {
-    // null は、localStorage に保存しない
     if (html) {
-        // localStorage は、0 から始まる
         localStorage.setItem(task, html);
         return;
     }
@@ -33,10 +30,8 @@ const deleteTaskFromLocalStorage = task => {
     return;
 }
 
-// ###############################
-
 const createTodoList = task => {
-    // HTML テンプレートを生成
+
     const html = `
     <li class="list-group-item">
         <span class="task-name">${task}</span>
@@ -45,20 +40,16 @@ const createTodoList = task => {
     `;
 
     $list.innerHTML += html;
-    // ########## 追加 ###########
     saveTaskToLocalStorage(task, html);
 }
 
 $addTask.addEventListener('submit', e => {
-    // デフォルトのイベントを無効
+
     e.preventDefault();
 
-    // タスクに入力した値を空白を除外して格納
     const task = $addTask.add.value.trim();
     if (task.length) {
-        // Todo List の HTML を作成
         createTodoList(task);
-        // タスクに入力した文字をクリア
         $addTask.reset();
     }
 });
@@ -76,29 +67,11 @@ $list.addEventListener('click', e => {
 $list.addEventListener('click', e => {
     if (e.target.classList.contains('delete')) {
         e.target.parentElement.remove();
-        // ########## 削除 ###########
         console.log($list.children[0].textContent.trim());
         const task = e.target.parentElement.textContent.trim()
         deleteTaskFromLocalStorage(task);
     }
 });
-
-// 全てを削除機能(拡張中)
-// $allDeleteBtn.addEventListener('click', () => {
-    
-//     const $list = document.querySelector('.todos');
-//     const listLength = $list.children.length;
-
-//     for(let i = 0; i < listLength ; i++){
-
-//         $list.children[i].remove();
-//         // ########## ストレージから削除 ###########
-//         const task = $list.children[i].textContent.trim();
-//         deleteTaskFromLocalStorage(task);
-
-        
-//     }    
-// });
 
 // リセット機能
 $allResetBtn.addEventListener('click', () => {
